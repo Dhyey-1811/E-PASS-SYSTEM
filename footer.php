dbfa379dbad2da3f46699d0ec3356ec78b81f4a7
Dhyey Patel <72376180+Dhyey-1811@users.noreply.github.com>
2021-11-22 08:42:21 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="footer-area section">
+        <div class="container">
+          
+            <div class="footer-bottom row align-items-center text-center text-lg-left">
+    <p>
+        Created by 19IT097 Dhyey Patel & 19IT099 Jatin Patel
+        From <b>CHARUSAT<b>
+    </p>
+            </div>
+        </div>
+    </footer>
